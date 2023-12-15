1f5323ab48157d968e48bbe266476896cc7e822d
Pasindu Perakum <it21160066my.sliit.lk>
2023-12-15 12:23:43 +0530

    changes

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,69 +1,89 @@
-import "./hero.scss"
-import { motion } from "framer-motion"
+import "./hero.scss";
+import { motion } from "framer-motion";
 const textVariants = {
-    initial: {
-        x: -500,
-        opacity: 0,
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
     },
-    animate: {
-        x: 0,
-        opacity: 1,
-        transition:{
-            duration: 1,
-            staggerChildren: 0.1,
-        },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
     },
-    scrollButton:{
-        opacity: 0,
-        y: 10,
-        transition:{
-            duration: 2,
-            repeat: Infinity
-        },
-    },
+  },
 };
 
 const sliderVariants = {
-    initial: {
-        x: 0,
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-220%",
+    transition: {
+      repeat: Infinity,
+      repeatType: "mirror",
+      duration: 20,
     },
-    animate: {
-        x: "-220%",
-        transition:{
-            repeat: Infinity,
-            repeatType: "mirror",
-            duration: 20,
-        },
-    },
+  },
 };
 
 const Hero = () => {
   return (
     <div className="hero">
-        <div className="wrapper">
-            <motion.div c
-                className="textConatiner" 
-                variants={textVariants} 
-                initial="initial"
-                animate="animate"
-            >
-                <motion.h2 variants={textVariants}>Pasindu Perkaum</motion.h2>
-                <motion.h1 variants={textVariants}>Software Engineer</motion.h1>
-                <motion.div variants={textVariants} className="buttons">
-                    <motion.button variants={textVariants}>See the Latest Work</motion.button>
-                    <motion.button variants={textVariants}>Contact Me</motion.button>
-                </motion.div>
-                <motion.img variants={textVariants} animate="scrollButton" src="/scroll.png" alt="" />
-            </motion.div>
-        </div>
-        <motion.div className="slidingTextContainer" variants={sliderVariants} initial="initial" animate="animate">
-            Full stack Software Engineer
+      <div className="wrapper">
+        <motion.div
+          c
+          className="textConatiner"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>Pasindu Perakum</motion.h2>
+          <motion.h1 variants={textVariants}>Software Engineer</motion.h1>
+          <motion.div variants={textVariants} className="buttons">
+            <motion.button variants={textVariants}>
+              See the Latest Work
+            </motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+          <motion.img
+            variants={textVariants}
+            animate="scrollButton"
+            src="/scroll.png"
+            alt=""
+          />
         </motion.div>
-        <div className="imageContainer">
-            <img src="./hero.png" alt="" />
-        </div>
+      </div>
+      <motion.div
+        className="slidingTextContainer"
+        variants={sliderVariants}
+        initial="initial"
+        animate="animate"
+      >
+        Full stack Software Engineer
+      </motion.div>
+      <div className="imageContainer">
+        <motion.img
+          src="./hero2.png"
+          alt=""
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 2 }}
+          transition={{ duration: 2 }}
+          whileHover={{ opacity: 1, scale: 1 }}
+        />
+      </div>
     </div>
-  )
-}//1.11
+  );
+}; //1.11
 
-export default Hero
+export default Hero;
