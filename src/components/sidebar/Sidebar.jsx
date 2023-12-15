fb44f841a71a26928d353924d5fee212cdf35102
Pasindu Perakum <it21160066my.sliit.lk>
2023-12-15 19:03:56 +0530

    chnages

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,16 +1,16 @@
-import Links from './links/Links'
-import ToggleButton from './toggleButton/ToggleButton'
-import './sidebar.scss'
-import { useState } from 'react'
-import { motion } from 'framer-motion'
+import Links from "./links/Links";
+import ToggleButton from "./toggleButton/ToggleButton";
+import "./sidebar.scss";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 const variants = {
   open: {
     clipPath: "circle(1200px at 50px 50px)",
-    transition: { 
+    transition: {
       type: "spring",
       stiffness: 20,
-    }
+    },
   },
   closed: {
     clipPath: "circle(30px at 50px 50px )",
@@ -19,20 +19,20 @@ const variants = {
       type: "spring",
       stiffness: 400,
       damping: 40,
-    }
-  }
+    },
+  },
 };
 const Sidebar = () => {
+  const [open, setOpen] = useState(false);
 
-  const [open, setOpen] = useState(false)
-
-
-  return <motion.div className="sidebar" animate={open ? "open" : "closed"}>
-    <motion.div className="bg" variants={variants}>
-      <Links/>
+  return (
+    <motion.div className="sidebar" animate={open ? "open" : "closed"}>
+      <motion.div className="bg" variants={variants}>
+        <Links />
+      </motion.div>
+      <ToggleButton setOpen={setOpen} />
     </motion.div>
-      <ToggleButton setOpen={setOpen}/>
-  </motion.div>
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
